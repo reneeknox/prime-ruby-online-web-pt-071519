cba8c7fa174589077662584af82148db78734535
Renee France <renee5.0@outlook.com>
2019-07-30 01:26:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -3,7 +3,7 @@ def is_prime(integer) # 10
     false
   else # (2..integer - 1).to_a
    # (2..10 - 1).to_a
-   
+   # [3,4,5,6,7,8,9]
    [].all? # Madeline told me this is a good idea to include; I read that it passes each element of the collection to the block. 
    # This helps us iterate over the range.
    
