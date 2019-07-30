e2b6d4605df9e1ede267123d7da93041be046883
Renee France <renee5.0@outlook.com>
2019-07-30 01:21:37 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -5,6 +5,7 @@ def is_prime(integer)
    integer % i == 0
    [].all? # Madeline told me this is a good idea to include; I read that it passes each element of the collection to the block. 
    # This helps us iterate over the range.
+   # 
   end
   true
 end
