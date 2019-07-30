974c1ea4518808ebd5716073c5b80b15ada95041
Renee France <renee5.0@outlook.com>
2019-07-30 01:20:56 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -4,7 +4,7 @@ def is_prime(integer)
   else # (2..integer - 1).to_a
    integer % i == 0
    [].all? # Madeline told me this is a good idea to include; I read that it passes each element of the collection to the block. 
-  i += 1
+   # This helps us iterate over the range.
   end
   true
 end
